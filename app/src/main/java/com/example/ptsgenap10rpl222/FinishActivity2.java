5ac1156c031dab50e95660fffd6af552a7d883e3
Gavin <m.gavin.aga10@gmail.com>
2021-03-19 06:27:48 +0700

    PTS GENAP

---
+++ app/src/main/java/com/example/ptsgenap10rpl222/FinishActivity2.java
@@ -0,0 +1,14 @@
+package com.example.ptsgenap10rpl222;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FinishActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_finish2);
+    }
+}
---
+++ app/src/main/java/com/example/ptsgenap10rpl222/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.ptsgenap10rpl222;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.text.Html;
+import android.text.Spanned;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import javax.security.auth.login.LoginException;
+
+import static android.text.Html.fromHtml;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText TxUsername, TxPassword;
+    Button BtnLogin;
+    Helper dbHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        TxUsername = (EditText)findViewById(R.id.txUsername);
+        TxPassword = (EditText)findViewById(R.id.txPassword);
+        BtnLogin = (Button)findViewById(R.id.btnLogin);
+
+        dbHelper = new Helper(this);
+
+        TextView tvCreateAccount = (TextView)findViewById(R.id.tvCreateAccount);
+
+
+
+        tvCreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, RegisterActivity2.class));
+            }
+        });
+
+        BtnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = TxUsername.getText().toString().trim();
+                String password = TxPassword.getText().toString().trim();
+
+                Boolean res = dbHelper.checkUser(username, password);
+                if (res == true){
+                    Toast.makeText(MainActivity.this, "Login Sukses", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(MainActivity.this, FinishActivity2.class));
+                }else {
+                    Toast.makeText(MainActivity.this, "Login Gagal coba lagi", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+
+    public static Spanned fromHtml (String html){
+        Spanned result;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N){
+            result = Html.fromHtml(html, Html.FROM_HTML_MODE_LEGACY);
+        }else {
+            result = Html.fromHtml(html);
+        }
+        return result;
+    }
+}
